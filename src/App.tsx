83bfae095e26e6bd05c3f62dddb6cee6851aa53b
Faizan Sadiq <139125421+fazi20@users.noreply.github.com>
2024-06-13 17:09:03 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,81 @@
+import { useState } from 'react'
+import './App.css'
+import Block from './Components/Block'
+import Swal from 'sweetalert2'
+
+
+function App() {
+
+  const [state, setState] = useState(Array(9).fill(null))
+  const [currentTurn, SetCurrentTurn] = useState("X")
+
+  const checkWinner = (state: any[]) => {
+
+    const win = [
+
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 6],
+      [0, 4, 8],
+      [2, 4, 6]
+
+
+    ]
+    for (let i = 0; i < win.length; i++) {
+
+      const [a, b, c] = win[i];
+      if (state[a] !== null && state[a] === state[b] && state[a] === state[c]) return true;
+    }
+  }
+
+  const HandleBlockClick = (index: number) => {
+    // console.log(index)
+    const stateCopy = Array.from(state);
+    if (stateCopy[index] !== null) return
+    stateCopy[index] = currentTurn;
+    SetCurrentTurn(currentTurn == "X" ? "O" : "X")
+    setState(stateCopy)
+
+    const win = checkWinner(stateCopy);
+    if (win) {
+      // alert()
+      Swal.fire(`${currentTurn} won the game`);
+     
+    }
+
+    console.log(stateCopy)
+
+  }
+  return (
+    <>
+    <h1>TIC-TAC-TOE</h1>
+
+
+    <div className='Board'>
+      <div className='row'>
+        <Block onClick={() => HandleBlockClick(0)} value={state[0]} />
+        <Block onClick={() => HandleBlockClick(1)} value={state[1]} />
+        <Block onClick={() => HandleBlockClick(2)} value={state[2]} />
+      </div>
+      <div className='row'>
+        <Block onClick={() => HandleBlockClick(3)} value={state[3]} />
+        <Block onClick={() => HandleBlockClick(4)} value={state[4]} />
+        <Block onClick={() => HandleBlockClick(5)} value={state[5]} />
+      </div>
+      <div className='row'>
+        <Block onClick={() => HandleBlockClick(6)} value={state[6]} />
+        <Block onClick={() => HandleBlockClick(7)} value={state[7]} />
+        <Block onClick={() => HandleBlockClick(8)} value={state[8]} />
+      </div>
+
+    </div>
+    </>
+  )
+}
+
+export default App
+
+
